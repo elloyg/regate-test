1388be53b1e8c09f887683c5783a78ecb44635e9
elloyg <guillaume.elloy@gmail.com>
2023-10-25 17:23:40 +0100

    feat: add login

--- src/screens/login-screen.tsx
+++ src/screens/login-screen.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, TextInput } from "react-native-paper";
 import styled from "styled-components/native";
 import { Theme } from "../theme";
 import { ScreenContainer } from "../components/layout";
+import { useRef, useState } from "react";
+import { LoginAPI } from "../apis/login-api";
+import { TextInput as TI } from "react-native";
 
-const Header = styled.View`
-  height: 300px;
+const Header = styled.SafeAreaView`
+  height: 200px;
   justify-content: center;
   align-items: center;
   background-color: ${Theme.colors.secondary};
@@ -15,22 +18,61 @@ const HeaderTitle = styled(Text)`
   color: white;
 `;
 
-const ButtonContainer = styled.View`
+const CenteredScrollView = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 48,
+  },
+})`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+`;
+
+const Field = styled(TextInput)`
+  align-self: stretch;
+  margin-bottom: 12px;
 `;
 
 export default function LoginScreen() {
+  const passwordRef = useRef<TI | null>(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function login() {
+    LoginAPI.login(email, password)
+      .then((user) => console.log(user))
+      .catch((e) => console.log(e));
+  }
+
+  const loginButtonDisabled = email === "" || password === "";
+
   return (
     <ScreenContainer>
       <StatusBar style="light" />
       <Header>
         <HeaderTitle variant="displaySmall">{"Regate Test"}</HeaderTitle>
       </Header>
-      <ButtonContainer>
-        <Button mode="contained">{"Login"}</Button>
-      </ButtonContainer>
+      <CenteredScrollView>
+        <Field
+          label="Email"
+          value={email}
+          onChangeText={(email) => setEmail(email.toLowerCase())}
+          keyboardType="email-address"
+          returnKeyType="next"
+          onBlur={() => passwordRef.current && passwordRef.current?.focus()}
+        />
+        <Field
+          ref={passwordRef}
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          returnKeyType="done"
+          secureTextEntry
+        />
+        <Button mode="contained" disabled={loginButtonDisabled} onPress={login}>
+          {"Login"}
+        </Button>
+      </CenteredScrollView>
     </ScreenContainer>
   );
 }
