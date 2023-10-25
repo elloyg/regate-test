0728a9a1209a97f693ca75e7fd4043af6345e92b
elloyg <guillaume.elloy@gmail.com>
2023-10-25 19:50:07 +0100

    fix: add apikey to get breeds

--- src/apis/cats-api.ts
+++ src/apis/cats-api.ts
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export type CatBreed = {
   id: string;
   name: string;
@@ -14,9 +16,30 @@ export type Cat = {
   breeds: CatBreed[];
 };
 
+// TO HIDE :D
+const API_KEY =
+  "live_RnoKlaAUknkiAjQGaOCuhuO5DHGL5fcd0pSDMjn4yxAkijB9tTo8JT2ZLL40W5vw";
+
 async function listRandomCats(limit: number = 10) {
-  const url = `https://api.thecatapi.com/v1/images/search?limit=${limit}`;
+  const url = `https://api.thecatapi.com/v1/images/search?has_breeds=true&limit=${limit}&api_key=${API_KEY}`;
   return (await fetch(url)).json() as Promise<Cat[]>;
 }
 
-export const CatsAPI = { listRandomCats };
+function useRandomCats() {
+  const [cats, setCats] = useState<Cat[] | undefined>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+
+  function fetch() {
+    setLoading(true);
+    setError(undefined);
+    listRandomCats()
+      .then(setCats)
+      .catch(() => setError("Error fetching your cats 😿"))
+      .finally(() => setLoading(false));
+  }
+
+  return { cats, fetch, loading, error };
+}
+
+export const CatsAPI = { listRandomCats, useRandomCats };
