5239a17954353469831772a6f862987665b7011b
elloyg <guillaume.elloy@gmail.com>
2023-10-25 17:02:13 +0100

    feat: add theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { MD3LightTheme } from "react-native-paper";
+
+export const Theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: "#EE8373",
+    secondary: "#172D53",
+    background: "#FAFCFE",
+  },
+};
